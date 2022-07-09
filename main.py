5e599f7e54ab52d736f5aaf77a93c2feafcd191b
pschloss <70105856+pschlo@users.noreply.github.com>
2022-07-10 00:42:38 +0200

    fixed GiB constant, concentrating on implementing WebDav; still unsure how to hande URIs

--- main.py
+++ main.py
@@ -1,38 +1,155 @@
-from typing import Any, Optional
+from typing import Any, Callable, Optional
+import time
 import requests as req
+import lxml.etree as etree
+import os
+import shutil
+import re
+from urllib.parse import unquote, urlsplit, urlparse
+
+# define datatypes
+Url = str
+Path = str
 
 def main():
+    '''
     config1 = HttpConfig([
         'https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_100KB_MP3.mp3',
-        'https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_500KB_MP3.mp3',
-        'https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_1OMB_MP3.mp3'
+        #'https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_500KB_MP3.mp3',
+        #'https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_1OMB_MP3.mp3'
         ], '')
+    '''
+    config1 = WebDavConfig(
+        root=r"https://cloud.rotex1880-cloud.org//remote.php/dav/files/backup/",
+        login='backup',
+        password=r";B-F$\4EeeQtVMjrZ.]r",
+        dest='D:/nextcloud-backup-test/')
     
     suite = BackupSuite(config1)
     suite.backup()
 
 
 
+    def get_url(self, path):
+        """Generates url by uri path.
+
+        :param path: uri path.
+        :return: the url string.
+        """
+        url = {'hostname': self.webdav.hostname, 'root': self.webdav.root, 'path': path}
+        return "{hostname}{root}{path}".format(**url)
+
+    def get_full_path(self, urn):
+        """Generates full path to remote resource exclude hostname.
+
+        :param urn: the URN to resource.
+        :return: full path to resource with root path.
+        """
+        return "{root}{path}".format(root=unquote(self.webdav.root), path=urn.path())
+
+    def get_rel_path(self, urn):
+        """Removes root path from URN.
+
+        :param urn: the URN to resource.
+        :return: relative path to resource without root path.
+        """
+        return re.sub(f'^{self.webdav.root}', '', urn.path())
+
 
 class BackupConfig:
-    sources: list[str]
-    dest: str
+    root: Url
+    get_sources: Callable[[], list[str]]
+    dest: Path
 
-    def __init__(self, sources: list[str], dest: str) -> None:
-        self.sources = sources
+    def __init__(self, root: Url, get_sources: Callable[[], list[str]], dest: Path) -> None:
+        self.root = root
+        self.get_sources = get_sources
         self.dest = dest
 
-    def backup(self) -> None:
+    def full_backup(self) -> None:
+        sources: list[str] = self.get_sources()
+        
+        source: str
+        for source in sources:
+            self.resource_backup(source, self.dest)
+    
+    def resource_backup(self, resource: str, dest: Path) -> None:
         raise NotImplementedError
 
 
 
+
+
+
+
 class WebDavConfig(BackupConfig):
-    def __init__(self, sources: list[str], dest: str) -> None:
-        super().__init__(sources, dest)
+
+    session: req.Session
+    login: str
+    password: str
+
+    def __init__(self, root: Url, login: str, password: str, dest: Path) -> None:
+        super().__init__(root, self.get_resources, dest)
+        self.login = login
+        self.password = password
+        self.session = req.Session()
+    
+    def get_resources(self) -> list[str]:
+        response = self.session.request(
+            method='PROPFIND',
+            url=self.root,
+            headers={"Accept": "*/*", "Depth": "99"},
+            auth=(self.login, self.password),
+            timeout=60,
+            verify=True
+            )
+        return self.parse_resource_list(response.content)
+
+    @staticmethod
+    def parse_resource_list(content: str | bytes) -> list[str]:
+        """Parses of response content XML from WebDAV server and extract file and directory names.
+
+        :param content: the XML content of HTTP response from WebDAV server for getting list of files by remote path.
+        :return: list of extracted file or directory names.
+        """
+        try:
+            root: etree.ElementBase = etree.fromstring(content, None)
+            urns: list[str] = []
+
+            # search for 'response' tag in entire tree in namespace 'DAV:'
+            response: etree.ElementBase
+            for response in root.findall(".//{DAV:}response", None):
+                href_elem: etree.ElementBase = response.find(".//{DAV:}href", None)
+                urns.append(href_elem.text)
+                a = urlsplit(href_elem.text)
+                #is_dir = len(response.findall(".//{DAV:}collection")) > 0
+                #urns.append(Urn(href, is_dir))
+            return urns
+        except etree.XMLSyntaxError:
+            return list()
+
+    def resource_backup(self, resource: str, dest: Path) -> None:
+        if os.path.exists(dest + resource):
+            #shutil.rmtree(dest + resource)
+            pass
+        os.makedirs(dest + resource)
+
+        '''response = self.session.request(
+            method='GET',
+            url=self.root + ,
+            headers={"Accept": "*/*", "Depth": "99"},
+            auth=(self.login, self.password),
+            timeout=60,
+            verify=True
+        )'''
 
 
 
+
+
+
+
+'''
 class HttpConfig(BackupConfig):
     session: req.Session
     login: str = ''
@@ -40,20 +157,26 @@ class HttpConfig(BackupConfig):
     verify: bool = True
     timeout: int = 30
 
-    def __init__(self, sources: list[str], dest: str) -> None:
+    root_url: Url
+    # whether we should mirror the source dir structure
+    mirror_dirs: bool = False
+
+    def __init__(self, root: Url, resources: list[Url], dest: Path) -> None:
         super().__init__(sources, dest)
         self.session = req.Session()
 
-    def backup(self) -> None:
+    def full_backup(self) -> None:
         for url in self.sources:
             # run HTTP GET request
             self.download_file(url)
             # store at dest
             pass
     
-    def download_file(self, url: str) -> None:
-        GiB: int = 2**10
+    def download_file(self, url: Url) -> None:
+        KiB: int = 2**10
+        MiB: int = 2**20
         local_filename: str = url.split('/')[-1]
+
         auth: Optional[tuple[str, str]]
         if self.login and self.password:
             auth = (self.login, self.password)
@@ -67,13 +190,13 @@ class HttpConfig(BackupConfig):
             'verify': self.verify,
             'timeout': self.timeout
         }
+        
         with self.session.get(**req_args) as r:
             # raise any HTTP errors
-            # print(r.headers)
             r.raise_for_status()
             # write file
             with open(local_filename, 'wb') as f:
-                for chunk in r.iter_content(chunk_size=8*GiB):
+                for chunk in r.iter_content(chunk_size=10*MiB):
                     f.write(chunk)
 
 
@@ -81,10 +204,10 @@ class HttpConfig(BackupConfig):
 
 
 class SshConfig(BackupConfig):
-    def __init__(self, sources: list[str], dest: str) -> None:
+    def __init__(self, sources: list[Url], dest: Path) -> None:
         super().__init__(sources, dest)
 
-
+'''
 
 class BackupSuite:
     configs: tuple[BackupConfig]
@@ -95,7 +218,7 @@ class BackupSuite:
 
     def backup(self):
         for config in self.configs:
-            config.backup()
+            config.full_backup()
 
 
 
